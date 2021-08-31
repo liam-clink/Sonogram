bbe7bb50a942cf480f6906c3f6e5bd618f20dcd9
Christoph Lauer <christophlauer@me.com>
2021-08-31 23:43:46 +0200

    reformated the WHOLE SOURCE CODE

--- source/src/de/dfki/sonogram/Licenses.java
+++ source/src/de/dfki/sonogram/Licenses.java
@@ -1,16 +1,14 @@
 package de.dfki.sonogram;
 
-public class Licenses 
-{
-    // This special Strings for the Licensing will be later exchanged by the webshop system    
+public class Licenses {
+  // This special Strings for the Licensing will be later exchanged by the webshop system
 
-    final static String zoda1 = "--- FREE FULLVERSION ---";     // name
-    final static String zoda2 = "";     // street and nr
-    final static String zoda3 = "";     // zip and city
-    final static String zoda4 = "";     // country
-    final static String zoda5 = "";     // email
-    final static String zoda6 = "";     // webpage
-    final static String zoda7 = "";     // phone
-    final static String zoda8 = "";     // note  
-
+  static final String zoda1 = "--- FREE FULLVERSION ---"; // name
+  static final String zoda2 = ""; // street and nr
+  static final String zoda3 = ""; // zip and city
+  static final String zoda4 = ""; // country
+  static final String zoda5 = ""; // email
+  static final String zoda6 = ""; // webpage
+  static final String zoda7 = ""; // phone
+  static final String zoda8 = ""; // note
 }
